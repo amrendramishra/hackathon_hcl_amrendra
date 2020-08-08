21d8283f26a63d08358b60a5cfe100b281a66b98
Amrendra <amrendra.narayan.mishra@gmail.com>
2020-08-08 06:54:47 +0530

    created project

---
+++ src/main/java/com/hackathon/Hackathon08AugSaturdayApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon08AugSaturdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon08AugSaturdayApplication.class, args);
+	}
+
+}
